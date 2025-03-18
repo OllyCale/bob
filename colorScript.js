ed9189bf73e34e18dc497c92900a2fe72473d37e
OllyCale <caleO1@hwbcymru.net>
2025-03-18 13:15:43 +0000

    chnged text colour

--- colorScript.js
+++ colorScript.js
@@ -1,7 +1,15 @@
 function Changecolor() {
-    const colors = ["#FFFFFF", "000000"];
+    const colors = ["#FFFFFF", "#000000", "wheat"];
     const RandomColor = colors[Math.floor(Math.random() * colors.length)];
 
     document.body.style.backgroundColor = RandomColor;
 
+    if (RandomColor === "#000000"){
+        document.getElementById("text").style.color = "#FFFFFF";
+        document.getElementById("title").style.color = "#FFFFFF";
+    }
+    if (RandomColor === "#FFFFFF" || RandomColor === "wheat");
+        {
+        document.getElementById("text").style.color = "#000000"
+    }
 }
