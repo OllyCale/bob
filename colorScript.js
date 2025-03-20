e5334b88ee282227c9bcc0ecd6171e20c8dd7a10
OllyCale <caleO1@hwbcymru.net>
2025-03-20 13:15:05 +0000

    update cool

--- colorScript.js
+++ colorScript.js
@@ -1,5 +1,5 @@
 function Changecolor() {
-    const colors = ["#FFFFFF", "#000000", "wheat"];
+    const colors = ["wheat", "FFFFFF", "000000", "ffb3b3", "ffccff", "b3e0ff", "c7ffb3", "ffeb99", "ffb366", "b3ffd9", "f2b3d6", "d4e2fc", "f2f2f2", "c2f0c2", "e3d6ff", "ffdfd3", "b3ffd6", "ff6666", "e3ffcc", "f7e6ff", "ccffcc"];
     const RandomColor = colors[Math.floor(Math.random() * colors.length)];
 
     document.body.style.backgroundColor = RandomColor;
@@ -8,8 +8,8 @@ function Changecolor() {
         document.getElementById("text").style.color = "#FFFFFF";
         document.getElementById("title").style.color = "#FFFFFF";
     }
-    if (RandomColor === "#FFFFFF" || RandomColor === "wheat");
-        {
-        document.getElementById("text").style.color = "#000000"
+    if (RandomColor === "#FFFFFF" || RandomColor === "wheat"){
+        document.getElementById("text").style.color = "#000000";
+        document.getElementById("title").style.color = "#000000";
     }
 }
